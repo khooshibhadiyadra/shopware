1370a62010b85e3d232ae534c9208e9979201673
khooshibhadiyadra <khooshi.bhadiyadra@icreativetechnolabs.com>
2025-04-23 10:59:15 +0530

    blogdefinition

--- custom/plugins/BlogDefinition/src/Core/Content/Blog/BlogBlogCategoryDefinition.php
+++ custom/plugins/BlogDefinition/src/Core/Content/Blog/BlogBlogCategoryDefinition.php
@@ -3,11 +3,9 @@
 namespace BlogDefinition\Core\Content\Blog;
 
 use BlogDefinition\Core\Content\BlogCategory\BlogCategoryDefinition;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\{FkField, Flag\PrimaryKey, Flag\Required, IdField};
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\MappingEntityDefinition;
-
 
 class BlogBlogCategoryDefinition extends MappingEntityDefinition
 {
@@ -21,10 +19,10 @@ public const ENTITY_NAME = 'blog_task_blog_category';
 //public function getCollectionClass(): string => BlogBlogCategoryCollection::class;
 protected function defineFields(): FieldCollection
 {
-return new FieldCollection([
-(new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
-(new FkField('blog_id', 'blogId', BlogDefinition::class))->addFlags(new Required()),
-(new FkField('blog_category_id', 'blogCategoryId', BlogCategoryDefinition::class))->addFlags(new Required()),
-]);
+    return new FieldCollection([
+    (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
+    (new FkField('blog_id', 'blogId', BlogDefinition::class))->addFlags(new Required()),
+    (new FkField('blog_category_id', 'blogCategoryId', BlogCategoryDefinition::class))->addFlags(new Required()),
+    ]);
 }
 }
