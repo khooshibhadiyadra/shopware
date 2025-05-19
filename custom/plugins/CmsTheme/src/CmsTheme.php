c169f30ee4de73987dfd144c8ada35db0ad3f5ef
khooshibhadiyadra <khooshi.bhadiyadra@icreativetechnolabs.com>
2025-05-19 18:58:14 +0530

    updated

---
+++ custom/plugins/CmsTheme/src/CmsTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace CmsTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class CmsTheme extends Plugin implements ThemeInterface
+{
+}
---
+++ custom/plugins/CmsTheme/src/Resources/app/administration/src/module/sw-cms/blocks/text-image/my-image-text-reversed/component/index.js
@@ -0,0 +1,11 @@
+import template from './sw-cms-block-my-image-text-reversed.html.twig';
+import './sw-cms-block-my-image-text-reversed.scss';
+// import './';
+
+Shopware.Component.register('sw-cms-block-my-image-text-reversed',{
+    template
+
+});
+// Shopware.Service('cmsService').registerCmsBlock({
+//
+// });
