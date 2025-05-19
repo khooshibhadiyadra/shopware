d2ac18b960121ee2d99d8f8528638056e4803b89
khooshibhadiyadra <khooshi.bhadiyadra@icreativetechnolabs.com>
2025-05-19 15:04:13 +0530

    themetask

---
+++ custom/plugins/CustomTheme/src/CustomTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace CustomTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class CustomTheme extends Plugin implements ThemeInterface
+{
+}
