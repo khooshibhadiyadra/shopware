5c5a1f1a9a4f6918eb3d78128a6237d51efd0179
khooshibhadiyadra <khooshi.bhadiyadra@icreativetechnolabs.com>
2025-05-20 19:03:53 +0530

    updated

--- custom/plugins/CmsTheme/src/Resources/app/administration/src/main.js
+++ custom/plugins/CmsTheme/src/Resources/app/administration/src/main.js
@@ -1,1 +1,2 @@
-import './module/sw-cms/blocks/text-image/my-image-text-reversed';
+import './module/sw-cms/blocks/text-image/custom-image-text-gallery';
+import './module/sw-cms/elements/buy-button';
---
+++ custom/plugins/CmsTheme/src/Resources/app/administration/src/module/sw-cms/elements/buy-button/component/index.js
@@ -0,0 +1,32 @@
+import template from "./sw-cms-el-buy-button.html.twig";
+import "./sw-cms-el-buy-button.scss";
+
+const { Mixin } = Shopware;
+
+Shopware.Component.register("sw-cms-el-buy-button", {
+    template,
+
+    compatConfig: Shopware.compatConfig,
+
+    mixins: [Mixin.getByName("cms-element"), Mixin.getByName("placeholder")],
+
+    computed: {
+        name() {
+            return this.element?.config?.name?.value ?? "shop";
+        },
+        redirectTo() {
+            return this.element.config?.link?.value ?? null;
+        },
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    methods: {
+        createdComponent() {
+            this.initElementConfig("buy-button");
+            this.initElementData("buy-button");
+        },
+    },
+});
